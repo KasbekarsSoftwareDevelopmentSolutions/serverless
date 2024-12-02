b80c1bd7d8db8effe993cf38cdf38425793711a2
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-12-02 14:23:31 -0500

    assignmnet 9 chnages.

--- src/main/java/com/cloudcomputing/verificationservice/repo/VerificationTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cloudcomputing.verificationservice.repo;
-
-import com.cloudcomputing.verificationservice.model.VerificationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-}
